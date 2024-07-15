7a0e0ee470265de2a7c02ed129d161d26aaff844
musiur <musiur.opu@gmail.com>
2024-07-16 05:21:32 +0600

    propt updated

--- app.py
+++ app.py
@@ -29,18 +29,18 @@ Hey Act Like a skilled or very experience ATS(Application Tracking System)
 with a deep understanding of tech field,software engineering,data science ,data analyst
 and big data engineer. Your task is to evaluate the resume based on the given job description.
 You must consider the job market is very competitive and you should provide 
-best assistance for improving thr resumes. Assign the percentage Matching based 
+best assistance for improving the resumes. Assign the percentage Matching based 
 on Jd and
 the missing keywords with high accuracy
 resume:{text}
 description:{jd}
-
-I want the response in one single string having the structure
-{{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
 """
 
+# I want the response in one single string having the structure
+# {{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
+
 ## streamlit app
-st.title("Smart ATS")
+st.title("Smart ATS - CSE499B.17_Group1")
 st.text("Improve Your Resume ATS")
 jd=st.text_area("Paste the Job Description")
 uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
